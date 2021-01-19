80a4de77d707049e6739bda73ab891e2eb77645b
Teddy Aleksov <teddy57603@gmail.com>
2021-01-19 18:56:03 +0200

    Add project files.

---
+++ 02.AverageStudentGrades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (students.ContainsKey(input[0]))
+                {
+                    students[input[0]].Add(double.Parse(input[1]));
+                }
+                else
+                {
+                    students.Add(input[0], new List<double>());
+                    students[input[0]].Add(double.Parse(input[1]));
+                }
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value)} (avrg: {(student.Value.Sum() / student.Value.Count):f2})");
+            }
+        }
+    }
+}
---
+++ SetsandDictionaries/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsandDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] arr = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> number = new Dictionary<double, int>(); 
+
+            foreach (double item in arr)
+            {
+                if (number.ContainsKey(item))
+                {
+                    number[item]++;
+                }
+                else
+                {
+                    number.Add(item, 1);
+                }
+            }
+
+            foreach (var item in number)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} times");
+            }
+        }
+    }
+}
